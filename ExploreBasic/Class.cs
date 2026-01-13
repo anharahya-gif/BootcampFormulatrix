0ec3295a89195a9f2202324e6315e0930f58f9ae
anharahya <anharahya@gmail.com>
2026-01-13 14:30:19 +0700

    add ExploreBasic

---
+++ ExploreBasic/Class.cs
@@ -0,0 +1,93 @@
+public interface IDriveable
+    {
+          public string brand {get;set;}
+         public int price {get;set;}
+
+        void Drive();
+        void Move();
+    }
+ public interface IRideable
+    {
+        void Ride();
+    }
+public class Car : Vehicle, IDriveable
+{
+   public string brand {get;set;}
+   public int price {get;set;}
+
+   public void Drive()
+    {
+        Console.WriteLine("Mobil Berjalan.");
+    } 
+      public override void Move()
+    {
+        Console.WriteLine("Mobil jalan");
+    }
+}
+public class Motorcycle : Vehicle
+{
+   public string brand {get;set;}
+   public int price {get;set;}
+
+   public void Ride()
+    {
+        Console.WriteLine("Motor Berjalan.");
+    } 
+      public override void Move()
+    {
+        Console.WriteLine("Motor jalan");
+    }
+}
+
+public class BankAccount
+{
+    private int _balance;
+
+    public void Deposit(int amount)
+    {
+        _balance += amount;
+    }
+
+    public int GetBalance()
+    {
+        return _balance;
+    }
+}
+
+public class Vehicle
+{
+    public virtual void Move()
+    {
+        Console.WriteLine("Kendaraan bergerak");
+    }
+}
+
+public class Boat : Vehicle
+{
+}
+
+class Mahasiswa
+{
+    public string Nama { get; set; }
+    public string NIM { get; set; }
+
+    public void TampilkanData()
+    {
+        Console.WriteLine($"Nama: {Nama}, NIM: {NIM}");
+    }
+}
+
+
+
+class Produk
+{
+    public string Nama { get; set; }
+    public int Harga { get; set; }
+
+    public Produk(string nama, int harga)
+    {
+        Nama = nama;
+        Harga = harga;
+    }
+}
+
---
+++ ExploreBasic/Program.cs
@@ -0,0 +1,64 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//How to use class method interface
+
+IDriveable myCar =  new Car();
+myCar.brand = "Toyota";
+myCar.price = 10000;
+Console.WriteLine(myCar.brand);
+Console.WriteLine(myCar.price);
+myCar.Drive();
+
+Motorcycle myMotor =  new Motorcycle();
+myMotor.brand = "Honda";
+myMotor.price = 1000;
+Console.WriteLine(myMotor.brand);
+Console.WriteLine(myMotor.price);
+myMotor.Ride();
+
+//Encapsulation
+BankAccount myBankAccount = new BankAccount();
+myBankAccount.Deposit(10010);
+Console.WriteLine(myBankAccount.GetBalance());
+
+//Inheritance and Polymorphism
+myCar.Move();
+myMotor.Move();
+
+Vehicle v;
+v = new Car();
+v.Move();
+
+v = new Motorcycle();
+v.Move();
+
+v = new Boat();
+v.Move();
+
+//Simply Class
+Mahasiswa mhs = new Mahasiswa();
+mhs.Nama = "Andi";
+mhs.NIM = "12345";
+mhs.TampilkanData();
+
+//Constructor
+Produk myProduct = new Produk("Baju", 100);
+Console.WriteLine(myProduct.Nama);
+Console.WriteLine(myProduct.Harga);
+
+//Basic Syntax
+// How to use Keyword as identifier use "@" example using,Int,class,public
+int @using = 123;
+Console.WriteLine(@using);
+// Contextual keyword bisa menjadi keyword atau identifier tergantung konteks. Example var,async,await,get,set
+int var = 123;
+Console.WriteLine(var);
+//Literal adalah nilai langsung di dalam kode. example 10,3.14,"Hello",true
+
+
+// Ini komentar
+/* Ini komentar
+   lebih dari satu baris */
+
