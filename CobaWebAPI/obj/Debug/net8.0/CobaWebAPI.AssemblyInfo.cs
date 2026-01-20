2413e857ef63a90300fbfcb73b311348446d6684
anharahya <anharahya@gmail.com>
2026-01-20 15:01:27 +0700

    update 20/01/2026

--- CobaWebAPI/obj/Debug/net8.0/CobaWebAPI.AssemblyInfo.cs
+++ CobaWebAPI/obj/Debug/net8.0/CobaWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CobaWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d61b71a44498e950f0724aad5d313e3f3ed3b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89b0cafb5fd73f9fafe3e44d55766ad5b34118f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CobaWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CobaWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MiniBoardgame/obj/Debug/net8.0/MiniBoardgame.AssemblyInfo.cs
+++ MiniBoardgame/obj/Debug/net8.0/MiniBoardgame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniBoardgame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42aa9a851a91f4fa227aed99290e2186a39f8392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89b0cafb5fd73f9fafe3e44d55766ad5b34118f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniBoardgame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniBoardgame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
